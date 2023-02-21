f83b8c3a4d07c81a020deadeb5f51c6cced66dd6
Cole Risch <metal.flow@gmail.com>
2023-02-20 20:24:37 -0800

    everything works!  gotta get the stats ready for turn in.

--- _8QueensSolver.py
+++ _8QueensSolver.py
@@ -1,9 +1,12 @@
-import sys,random,math
+import random,math,time
 
 #set constants
-#POPULATIONSIZES={10,100,500,1000}
-POPULATIONSIZES={10}
+POPULATIONSIZES={10,100,500,1000}
 MUTATIONFACTOR=50
+MAXPERFORMANCE=56
+
+#initialize variables
+NumIterations = 100
 
 #define classes
 class Solution:
@@ -13,7 +16,7 @@ class Solution:
         self.data = []
         while len(workingList)>0:
             self.data.append(workingList.pop(random.randint(0,len(workingList)-1)))
-        self.performance = 36
+        self.performance = MAXPERFORMANCE
         self.measurePerformance()
         #print(str(self.data) + " scored "+str(self.performance))
         del workingList
@@ -27,7 +30,7 @@ class Solution:
             if listIn.count(element)!=1:
                 raise Exception(str(len(listIn))+" elements provided, and 8 are required for creating a solution")
         self.data = listIn.copy()
-        self.performance = 36
+        self.performance = MAXPERFORMANCE
         self.measurePerformance()
         #print(str(self.data) + " scored "+str(self.performance))
         return
@@ -38,11 +41,11 @@ class Solution:
             #check columns for other attacking Queens
             #not needed since each queen has their own column
             #check rows for attacking Queens
-            #needed in case of gene splicing error
-            if self.data.count(currentQueen)>1:
-                self.performance = 0
-                print ("gene splicing error, discard")
-                return
+            #testing has shown that gene splicing errors do not occur, thus this isn't needed
+            #if self.data.count(currentQueen)>1:
+            #    self.performance = 0
+            #    print ("gene splicing error, discard")
+            #    return
             #check diaganols
             #this is kinda hard, what we need to do is check all other indices
             #and ensure that they are not equal to the value of the current number +/-
@@ -62,12 +65,26 @@ class Solution:
             display[self.data.index(8-row)]="Q"
             print(display)
 
+
+
 #define functions
 def PerformanceSort(thisSolution):
     return thisSolution.performance
 
-#initialize variables
-NumIterations = 100
+def GatherStats(populationToAnalyze:list) -> list:
+    leastPerformance=MAXPERFORMANCE
+    bestPerformance=0
+    averagePerformance=0
+    for currentSolution in populationToAnalyze:
+        if currentSolution.performance < leastPerformance:
+            leastPerformance = currentSolution.performance
+        if currentSolution.performance > bestPerformance:
+            bestPerformance = currentSolution.performance
+        averagePerformance+=currentSolution.performance
+    averagePerformance=averagePerformance/len(populationToAnalyze)
+    return [leastPerformance,bestPerformance,averagePerformance,time.time()]
+
+
 
 #begin program
 for populationSize in POPULATIONSIZES:
@@ -75,13 +92,18 @@ for populationSize in POPULATIONSIZES:
     
     #create initial populzation of random solutions
     population=[Solution() for _ in range(populationSize)]
+    #create inital stats list
+    stats = []
 
     #sort by performance score
     population.sort(reverse=False,key=PerformanceSort)
 
+    #gather initial stats
+    stats.append(GatherStats(population))
+
     #begin GA iterations
     for iteration in range(0,NumIterations):
-        print("Iteration #"+str(iteration))
+        print(str(iteration),end =",")
 
         #create new population
         newGeneration = []
@@ -90,13 +112,19 @@ for populationSize in POPULATIONSIZES:
         #so I updated this section to copy forward any existing
         #solutions before performing any GA calcs, this also
         #trims out duplicate solutions per generation to reduce
-        #overeall duplicate population while also setting up 
+        #overall duplicate population while also setting up 
         ##the normalization factor
         normFactor=0
         for currentSolution in population:
             normFactor+=currentSolution.performance
-            if currentSolution.performance == 36:
-                newGeneration.append(currentSolution)
+            if currentSolution.performance == MAXPERFORMANCE:
+                dupeFlag = True
+                for comparisonSolution in newGeneration:
+                    if str(currentSolution.data)==str(comparisonSolution.data):
+                        dupeFlag = False
+                        break
+                if dupeFlag:
+                    newGeneration.append(currentSolution)
                 #newGenIndex+=1
             
         #time to make a new generation
@@ -133,7 +161,7 @@ for populationSize in POPULATIONSIZES:
             #dictated by the perforamnce, where best perforamnce allows
             #for a locus  up to largest index, and worst allows only
             #the smallest
-            maxIndex=math.floor((len(parent1.data)-1)-(((32-parent1.performance)/32)*(len(parent1.data)-1)))
+            maxIndex=math.floor((len(parent1.data)-1)-(((MAXPERFORMANCE-parent1.performance)/MAXPERFORMANCE)*(len(parent1.data)-1)))
             childData=[]
             for currentNum in range(0,random.randint(0,maxIndex)):
                 childData.append(parent1.data[currentNum])
@@ -172,14 +200,28 @@ for populationSize in POPULATIONSIZES:
         population = newGeneration.copy()
         population.sort(reverse=False,key=PerformanceSort)
         del newGeneration
-
+        #be sure to regather stats
+        stats.append(GatherStats(population))
+    print("complete!")
     #sort by performance score
     population.sort(reverse=True,key=PerformanceSort)
 
     #display our final results
-    for currentSolution in population:
-        print("solution "+str(currentSolution.data)+" has performance score of "+str(currentSolution.performance))
-        if currentSolution.performance == 36:
-            currentSolution.DisplaySolution()
-        
+    #for currentSolution in population:
+    #    print("solution "+str(currentSolution.data)+" has performance score of "+str(currentSolution.performance))
+    #    if currentSolution.performance == MAXPERFORMANCE:
+    #        currentSolution.DisplaySolution()
+    statCounter=0
+    totalTime=0
+    for stat in stats:
+        if stats.index(stat) == 0:
+            print("Generation:"+str(statCounter)+" Least Performance:"+str(stat[0])+" Best Performance:"+str(stat[1])+" Average Performance:"+str(stat[2]))
+        else:
+            perTime=stat[3]-stats[statCounter-1][3]
+            totalTime+=perTime
+            print("Generation:%d Least Performance:%d Best Performance:%d Average Performance:%f time per generation:%1.4f"%(statCounter,stat[0],stat[1],stat[2],perTime))
+        statCounter+=1
+    averageTime=totalTime/(len(stats)-1)
+    print("average time per generation:"+str(averageTime)+" seconds")
+    del stats
     del population
